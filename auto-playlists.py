b01b1f5af9105c765dcc79c47f834dded56d071b
mcarlton00 <mcarlton00@gmail.com>
2019-12-09 10:18:37 -0500

    Generate recently added playlist (semi-working)

--- auto-playlists.py
+++ auto-playlists.py
@@ -3,16 +3,18 @@ import requests
 from pprint import pprint
 import yaml
 
-class playlists():
+class Playlists():
     def __init__(self):
-        self.login()
-
-    def login(self):
         self.data = self.read_file()
-        user_id = self.data.get('auth').get('user_id')
-        username = self.data.get('auth').get('user')
-        password = self.data.get('auth').get('password')
-        api_url = self.data.get('auth').get('server')
+        self.login(self.data)
+
+    def login(self, data):
+        user_id = self.data.get('server_data').get('user_id')
+        username = self.data.get('server_data').get('user')
+        password = self.data.get('server_data').get('password')
+        api_url = self.data.get('server_data').get('server')
+        self.music_library = self.data.get('server_data').get(
+            'music_library', 'Music')
 
         auth_data = {
             'Username': username,
@@ -31,7 +33,6 @@ class playlists():
 
         headers['x-emby-authorization'] = authorization
 
-        api_url = 'https://streaming.nerdyredneck.net'
         auth_url = f'{api_url}/Users/AuthenticateByName'
 
         r = requests.post(auth_url, headers=headers, data=auth_data)
@@ -51,22 +52,27 @@ class playlists():
 
         return data
 
-    def get_library_id(self):
+    def get_library_id(self, library_name):
         r = requests.get(f'{self.api_url}/Users/{self.user_id}/Views',
                          headers=self.headers)
+        r.raise_for_status()
         libraries = r.json().get('Items')
-        pl_library_id = [ x.get('Id') for x in libraries
-                        if x.get('Name') == 'Playlists']
-        return pl_library_id
+        library_id = [ x.get('Id') for x in libraries
+                        if x.get('Name') == library_name]
+        if library_id:
+            library_id = library_id[0]
+
+        return library_id
 
     def get_all_playlists(self):
-        self.pl_library_id = self.get_library_id()
-        r = requests.get(f'{self.api_url}/Items?ParentId={self.pl_library_id}'
+        library_id = self.get_library_id('Playlists')
+        r = requests.get(f'{self.api_url}/Items?ParentId={library_id}'
                          f'&UserId={self.user_id}&IncludeItemTypes=Playlist'
                          f'&Recursive=true', headers=self.headers)
+        r.raise_for_status()
 
         existing_playlists = r.json().get('Items')
-        playlists = [{i.get('Name'): i.get('Id')} for i in existing_playlists ]
+        playlists = {i.get('Name'): i.get('Id') for i in existing_playlists }
         return playlists
 
 
@@ -75,12 +81,90 @@ class playlists():
         r = requests.post(f'{self.api_url}/Playlists?UserId={self.user_id}'
                           f'&Name={playlist_name}', headers=self.headers,
                           data=data)
+        r.raise_for_status()
 
+    def add_single_to_playlist(self, playlist_id, item_id):
+        r = requests.post(f'{self.api_url}/Playlists/{playlist_id}/Items?'
+                            f'Ids={item_id}&UserId={self.user_id}',
+                            headers=self.headers)
+        r.raise_for_status()
+
+    def add_bulk_to_playlist(self, playlist_id, items):
+        item_ids = [ item.get('Id') for item in items ]
+        item_ids = ','.join(item_ids)
+
+        r = requests.post(f'{self.api_url}/Playlists/{playlist_id}/Items?'
+                            f'Ids={item_ids}&UserId={self.user_id}',
+                            headers=self.headers)
+        r.raise_for_status()
+
+    def get_playlist_id(self, playlist_name):
+        playlists = self.get_all_playlists()
+
+        playlist_id = playlists.get(playlist_name)
+
+        return playlist_id
+
+    def get_playlist_contents(self, playlist_name):
+        playlist_id = self.get_playlist_id(playlist_name)
+
+        r = requests.get(f'{self.api_url}/Playlists/{playlist_id}/Items?'
+                         f'UserId={self.user_id}', headers=self.headers)
+
+        r.raise_for_status()
+
+        return r.json().get('Items')
+
+    def clear_playlist(self, playlist_name):
+        playlist_id = self.get_playlist_id(playlist_name)
+        tracks = self.get_playlist_contents(playlist_name)
+
+        tracklist = [track.get('Id') for track in tracks ]
+
+        grouped_ids = list(self.split_list(tracklist, 15))
+        for group in grouped_ids:
+            item_ids = ','.join(group)
+            #print(item_ids)
+
+
+        #return item_ids
+
+            print(f'{self.api_url}/Playlists/{playlist_id}/Items?'
+                              f'UserId={self.user_id}&EntryIds={item_ids}')#,
+            r = requests.delete(f'{self.api_url}/Playlists/{playlist_id}/Items?'
+                              f'UserId={self.user_id}&EntryIds={item_ids}',
+                              headers=self.headers)
+            print(r.status_code)
+            r.raise_for_status()
+
+    def split_list(self, item_ids, size):
+        for i in range(0, len(item_ids), size):
+            yield item_ids[i:i+size]
+
+    def get_all_tracks(self):
+        library_id = self.get_library_id(self.music_library)
+
+        r = requests.get(f'{self.api_url}/Users/{self.user_id}/Items?'
+                         f'ParentId={library_id}&Recursive=true&'
+                         f'IncludeItemTypes=Audio&'
+                         f'Fields=Genres', headers=self.headers)
+
+        r.raise_for_status()
+
+        tracks = r.json().get('Items')
+
+        return tracks
+
+    def get_recent_tracks(self, num):
+        library_id = self.get_library_id(self.music_library)
+
+        r = requests.get(f'{self.api_url}/Users/{self.user_id}/Items/Latest?'
+                         f'IncludeItemTypes=Audio&Limit={num}&Recursive=true&'
+                         f'ParentId={library_id}', headers=self.headers)
+
+        r.raise_for_status()
+
+        return r.json()
 
 if __name__ == '__main__':
-    thing = playlists()
-
-# TODO
-# Add items to a playlist
-# make sure to check for duplicates before adding
-#https://jellyfin.example.com/Playlists/65e0a0d4bc429a24b987a14c29202983/Items?EntryIds=0e098cbf4dbadebb07ae26a5242ad01a%2Cace016257fa209a68d05351828c011a9&UserId=02722887cbe547f49ca42b5c12e61f4b&format=json
+    generator = Playlists()
